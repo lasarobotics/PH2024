5bbda1b376fbdc2b8bae1c2d36a0c03db722c455
rachitkakkar <rachitkakkar7@gmail.com>
2024-01-11 09:50:59 -0600

    Cleanup constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,8 +96,8 @@ public final class Constants {
   }
 
   public static class ShooterHardware {
-    public static final Spark.ID LEFT_SLAVE_MOTOR_ID = new Spark.ID("DriveHardware/Shooter/Left", 11);
-    public static final Spark.ID RIGHT_MASTER_MOTOR_ID = new Spark.ID("DriveHardware/Shooter/Right", 12);
+    public static final Spark.ID LEFT_SLAVE_MOTOR_ID = new Spark.ID("ShooterHardware/Shooter/Left", 11);
+    public static final Spark.ID RIGHT_MASTER_MOTOR_ID = new Spark.ID("ShooterHardware/Shooter/Right", 12);
     
   }
 
