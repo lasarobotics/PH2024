7260a9e82767bc0c145c0d6a98dc47f50b21c134
rachitkakkar <rachitkakkar7@gmail.com>
2024-01-11 20:16:20 -0600

    Add Empty Climber Subsystem

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberSubsystem.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.climber;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ClimberSubsystem extends SubsystemBase {
+  /** Creates a new ClimberSubsystem. */
+  public ClimberSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
