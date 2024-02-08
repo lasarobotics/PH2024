96a5e5b63ff2ba4aa7cc27b977eb7c8f8a5ee8a1
David-Dube <117662634+David-Dube@users.noreply.github.com>
2024-02-08 16:37:42 -0600

    Added an auto

---
+++ src/main/java/frc/robot/commands/autonomous/TestAuto.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.drive.AutoTrajectory;
+import frc.robot.subsystems.drive.DriveSubsystem;
+
+public class TestAuto extends SequentialCommandGroup {
+  /** Creates a new Leave auto */
+  public TestAuto(DriveSubsystem driveSubsystem) {
+    AutoTrajectory auto = new AutoTrajectory(driveSubsystem, "test");
+
+    addCommands(
+      auto.getCommand()
+    );
+  }
+}
